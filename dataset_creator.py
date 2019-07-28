1f6cb871aa3e855aaa9432164c94fce45bb2f8bc
Freddy Vandalay <fsund001@gmail.com>
2019-07-28 12:59:06 +0100

    *Added description *Formatted strings *print --> logging

--- dataset_creator.py
+++ dataset_creator.py
@@ -1,9 +1,31 @@
+"""
+
+This script copy files from a specified folders subdirectories into a new specified path. 
+The user can specify the number fo files to be copied to the new paths subdirectories.
+The files are picked randomly.
+
+The export path is created if it does not exist
+
+__root export path
+	|
+	|__subdir 1
+	|__subdir 2
+	|__subdir 3
+	|__... 
+
+
+***Orinally created to create datasets for model training with TensorFlow 
+
+
+"""
+import argparse
+import logging
+import numpy as np
 import os
+import random 
 import sys 
 import shutil
-import argparse
-import random 
-import numpy as np
+
 
 FLAGS = None 
 
@@ -12,19 +34,19 @@ parser = argparse.ArgumentParser()
 parser.add_argument(
 	'--import_path',
     type=str,
-    default='import_folder/',
+    default='import_folder',
     help='Location of import directory')
 
 parser.add_argument(
 	'--export_path',
     type=str,
-    default='export_folder/',
+    default='export_folder',
     help='Location of export directory')
 parser.add_argument(
 	'--samples',
     type=int,
     default='200',
-    help='Location of export directory')
+    help='Specifies the number of samples to be saved in the directory')
 FLAGS, unparsed = parser.parse_known_args()
 
 IMPORT_PATH = FLAGS.import_path 
@@ -39,23 +61,26 @@ def __MAIN__():
 def generate_dataset():
 	root_dir=IMPORT_PATH
 	for subdir in os.listdir(root_dir):
-		current_folder = EXPORT_PATH + '/'+ subdir + '_' + str(SAMPLES) + '/'
+		files_in_path = os.listdir("{}/{}/".format(IMPORT_PATH, subdir))
+		export_subdirectory = "{}/{}_{}".format(EXPORT_PATH, subdir, SAMPLES)
 		
-		makeDir(EXPORT_PATH + '/'+ subdir + '_' + str(SAMPLES) + '/')
+		makeDir(export_subdirectory)
 		
-		print('Processing: ')
-		print(subdir)
-		while len(os.listdir(current_folder))<SAMPLES:
-			files = os.listdir(str(IMPORT_PATH + '/' + subdir + '/'))
-			np.random.shuffle(files)
-			exported_files = os.listdir(current_folder)
-			pick_if_one=random.randint(0,len(files)-1)
+		logging.info("Processing subdirectory '{}'...".format(subdir))
+		while len(os.listdir(export_subdirectory)) < SAMPLES:
+			np.random.shuffle(files_in_path)
+			exported_files = os.listdir(export_subdirectory)
+			pick_if_one=random.randint(0,len(files_in_path)-1)
 			if files[pick_if_one] not in exported_files:
-				shutil.copy(IMPORT_PATH + '/' + subdir + '/' + files[pick_if_one], current_folder + files[pick_if_one])
-	print(' files copied to ' + EXPORT_PATH)
+				shutil.copy("{}/{}".format(IMPORT_PATH, files[pick_if_one]), 
+					"{}/{}".format(export_subdirectory, files[pick_if_one]))
+	
+	logging.info("Files copied to {}".format(EXPORT_PATH))
 
 def makeDir(newDir):
-	if not os.path.exists(newDir): os.makedirs(newDir)
+	if not os.path.exists(newDir): 
+		logging.info("Created directory '{}'".format(newDir))
+		os.makedirs(newDir)
 	return newDir
 
 __MAIN__()
